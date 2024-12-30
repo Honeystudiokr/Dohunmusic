0bc572dc925e3ee685e7569982a35657825a3eee
도헌 <junny8476@naver.com>
2024-12-30 10:03:59 +0900

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -25,7 +25,7 @@ const page = async () => {
             </div>
           }
           title="다시 듣기"
-          subTitle="도도"
+          subTitle="도헌뮤직"
         />
         <div className="mt-20"></div>
         <PlayListCarousel
